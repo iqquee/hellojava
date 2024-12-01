ee4b4d0d08cb50f1c76cfb2eeb72cffc2dd5c000
Mathias Jiya <onlyiqquee@gmail.com>
2024-12-01 19:53:13 +0100

    operators.java added

---
+++ operators.java
@@ -0,0 +1,30 @@
+public class operators {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        // Addition
+        int sum = a + b;
+        System.out.printf("Addutions: %d\n", sum);
+
+        // Subtraction
+        int sub = a - b;
+        System.out.printf("Subtraction: %d\n", sub);
+
+        // Division
+        int div = a / b;
+        System.out.printf("Division: %d\n", div);
+
+        // Modulus
+        int mod = a % b;
+        System.out.printf("Modulus: %d\n",mod);
+
+        // Post Increment
+        a++;
+        System.out.printf("Post Increment a: %d\n", a);
+
+        // Pre Increment
+        ++a;
+        System.out.printf("Pre Increment a: %d\n", a);
+    }
+}
